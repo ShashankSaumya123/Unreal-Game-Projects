02d74a2f24230900f6e11cc9db643e1fdb8194b5
ShashankSaumya123 <shashank.saumya@niser.ac.in>
2022-12-22 21:30:01 +0530

    lec 47 done

--- SastaFallGuys/Source/SastaFallGuys/MovingPlatform.h
+++ SastaFallGuys/Source/SastaFallGuys/MovingPlatform.h
@@ -24,12 +24,8 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	UPROPERTY(EditAnywhere)
-	int32 MyInt = 99;
+	FVector MyVector = FVector(1,2,3);
 
 	UPROPERTY(EditAnywhere)
-	float MyFloat = 5.32;
-
-	UPROPERTY(EditAnywhere)
-	bool MyBool = true;
-
+	float MyX = 0;
 };
